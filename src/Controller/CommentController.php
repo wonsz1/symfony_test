9034b581186cc3d10ad00f79753aef89c2dd0676
Mateusz Rusiecki <mateuszr87@gmail.com>
2025-04-22 17:08:50 +0200

    fix IsGranted attribute

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class CommentController extends AbstractController
 {
