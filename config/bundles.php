dcdbdbb2975a205cebbd109c02e7e4492f634dc4
Mateusz Rusiecki <mateuszr87@gmail.com>
2025-05-27 17:32:23 +0200

    add api platform;

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
