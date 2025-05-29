72739e627b49f69f4548c00431bdf9edb7c68bee
Mateusz Rusiecki <mateuszr87@gmail.com>
2025-05-29 11:40:52 +0200

    add api examples to Comment entity

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -11,6 +11,7 @@ use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Post as PostOperation;
 use ApiPlatform\Metadata\Put;
 use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\ApiProperty;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
 #[ApiResource(
@@ -34,15 +35,19 @@ class Comment
 
     #[ORM\Column(type: 'text')]
     #[Groups(['comment:read', 'comment:write'])]
+    #[ApiProperty(example: 'This is a great post! Thanks for sharing.')]
     private ?string $content = null;
 
     #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'comments')]
     #[ORM\JoinColumn(nullable: false)]
     #[Groups(['comment:read', 'comment:write'])]
+    #[ApiProperty(example: '/api/users/1')]
     private ?User $author = null;
 
     #[ORM\ManyToOne(targetEntity: Post::class, inversedBy: 'comments')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['comment:read', 'comment:write'])]
+    #[ApiProperty(example: '/api/posts/1')]
     private ?Post $post = null;
 
     #[ORM\Column]
@@ -57,15 +62,53 @@ class Comment
         $this->isApproved = false;
     }
 
-    public function getId(): ?int { return $this->id; }
-    public function getContent(): ?string { return $this->content; }
-    public function setContent(string $content): static { $this->content = $content; return $this; }
-    public function getAuthor(): ?User { return $this->author; }
-    public function setAuthor(?User $author): static { $this->author = $author; return $this; }
-    public function getPost(): ?Post { return $this->post; }
-    public function setPost(?Post $post): static { $this->post = $post; return $this; }
-    public function getCreatedAt(): ?\DateTimeImmutable { return $this->createdAt; }
-    public function setCreatedAt(\DateTimeImmutable $createdAt): static { $this->createdAt = $createdAt; return $this; }
-    public function isApproved(): bool { return $this->isApproved; }
-    public function setIsApproved(bool $isApproved): static { $this->isApproved = $isApproved; return $this; }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+        return $this;
+    }
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+    public function setAuthor(?User $author): static
+    {
+        $this->author = $author;
+        return $this;
+    }
+    public function getPost(): ?Post
+    {
+        return $this->post;
+    }
+    public function setPost(?Post $post): static
+    {
+        $this->post = $post;
+        return $this;
+    }
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+    public function isApproved(): bool
+    {
+        return $this->isApproved;
+    }
+    public function setIsApproved(bool $isApproved): static
+    {
+        $this->isApproved = $isApproved;
+        return $this;
+    }
 }
